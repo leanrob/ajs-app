33b42b546ff0107ca04ac995349fead39fa79f79
Robert Byrne <mr.rob.byrne@gmail.com>
2016-08-06 23:21:40 -0230

    Added $scope data to the page also added navbar
    Added some parts of the event object that is in the $scope of the program. Added some notes to this for future reference.
    
    Also divided things up into bootstrap rows and an image of the angular logo using the ng-src so that we could use the angular `{{}}` notation inside the alt text and image string.

--- app/js/controllers/EventController.js
+++ app/js/controllers/EventController.js
@@ -4,12 +4,22 @@
 
 'use strict';
 
+/*
+We can print anything to page that is within the $scope. Remove this comment after this commit.
+ */
+
 eventsApp.controller('EventController', function EventController($scope) {
 
     $scope.event = {
         name: 'Robert Byrne',
         date: '1/1/2013',
-        time: '10:30 am'
+        time: '10:30 am',
+        location: {
+            address: '251 Bay Street',
+            city: 'Toronto',
+            province: 'Canada'
+        },
+        imageUrl: '/img/angularjs-logo.png'
     }
 
 });
